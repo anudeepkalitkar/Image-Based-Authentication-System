d6ed711eeadda076e97b43f4620d4949d493f49f
Anudeep Kalitkar <50821904+anudeepkalitkar@users.noreply.github.com>
2024-02-06 07:14:53 -0700

    git page published

--- src/StaticInformation/UrlLinkInfo.js
+++ src/StaticInformation/UrlLinkInfo.js
@@ -3,11 +3,8 @@ const linkedinUrl = "https://www.linkedin.com/in/anudeep-kalitkar-7b816525b";
 const githubUrl = "https://github.com/anudeepkalitkar/";
 const gmailUrl = "mailto:anudeep.kalitkar@gmail.com";
 const PortFolioURL = "https://anudeepkalitkar.github.io/";
-// const BackEndLoginURL = "https://restapi-heroku-efe9457505f4.herokuapp.com/";
-// const BackEndSignupURL = "https://restapi-heroku-efe9457505f4.herokuapp.com/";
-const BackEndLoginURL = "http://127.0.0.1:5000/ibasiconlogin";
-const BackEndSignupURL = "http://127.0.0.1:5000/ibasiconregister";
-
+const BackEndLoginURL = "https://restapi-heroku-efe9457505f4.herokuapp.com/ibasiconlogin";
+const BackEndSignupURL = "https://restapi-heroku-efe9457505f4.herokuapp.com/ibasiconregister";
 
 export {
     instagramUrl,
